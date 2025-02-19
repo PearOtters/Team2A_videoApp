32996b27c5337fa2c63d14ae0374f264f9f74742
PearOtters <pierrealexis.outters@gmail.com>
2025-02-19 23:40:18 +0000

    small changes to view login requirements and renames in urls, added logout

--- video/views.py
+++ video/views.py
@@ -4,7 +4,6 @@ from video.models import Video, Category, Comment, UserProfile, Reply
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from datetime import datetime 
 
 def index(request):
     return render(request, 'video/index.html')
@@ -17,24 +16,27 @@ def view_categories(request):
     context_dict['categories'] = Category.objects.order_by('-video_count')
     return render(request, 'video/categories.html', context=context_dict)
 
-
 def category_detail(request):
     return HttpResponse("Under construction...")
 
 def video_detail(request):
     return HttpResponse("Under construction...")
 
-def login_view(request):
-    return HttpResponse("Under construction...")
-
-def logout_view(request):
+def user_login(request):
     return HttpResponse("Under construction...")
 
 def register(request):
     return HttpResponse("Under construction...")
 
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('video:index'))
+
+@login_required
 def upload(request):
     return HttpResponse("Under construction...")
 
+@login_required
 def add_category(request):
     return HttpResponse("Under construction...")
